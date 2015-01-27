f19420c33cc68d8a00cebd32cf62428d29e66143
Vivian Liang <vliang88@gmail.com>
2015-01-27 15:09:01 -0800

    ModelSerializers. Regular Django views using our Serializer.

--- pastebin/pastebin/urls.py
+++ pastebin/pastebin/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from snippets import views
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pastebin.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^', include('snippets.urls')),
+
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ pastebin/snippets/serializers.py
@@ -0,0 +1,10 @@
+from django.forms import widgets
+from rest_framework import serializers
+
+from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
