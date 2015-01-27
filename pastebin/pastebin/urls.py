785850cbbb2dd24e4e35afd8bc837f142b6d7f59
Vivian Liang <vliang88@gmail.com>
2015-01-27 14:41:44 -0800

    Create serializer class

--- pastebin/pastebin/urls.py
+++ pastebin/pastebin/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'pastebin.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include('snippets.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
