c1f67b21b2a323330add2e59672e1882fac377d3
Vivian Liang <vliang88@gmail.com>
2015-01-27 20:18:16 -0800

    Add endpoints for our user models. Associate Snippets with Users

--- pastebin/snippets/serializers.py
+++ pastebin/snippets/serializers.py
@@ -1,10 +1,18 @@
-from django.forms import widgets
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+from snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- pastebin/snippets/views.py
+++ pastebin/snippets/views.py
@@ -1,5 +1,6 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 from rest_framework import generics
 
@@ -11,6 +12,9 @@ class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
 
+  def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   """
@@ -18,3 +22,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   """
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
