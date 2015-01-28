2159997420a7a6de1db8695e5335b85000c649bd
Vivian Liang <vliang88@gmail.com>
2015-01-27 20:38:32 -0800

    Add login to browsable API. Use login link. Navigate to /users/ endpoint. List includes list of snippet pks associated with each user in each user's snippets fields.

--- pastebin/pastebin/urls.py
+++ pastebin/pastebin/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
